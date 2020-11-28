3bd59a271210a7080d3484901f6b67cf0b4a0793
thebestlucc <thebestlucc@gmail.com>
2020-11-27 23:15:12 -0300

    Add Link to mugiwara-logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Logo from "../../assets/imgs/mugiwara.svg";
 import Banner from "../../assets/imgs/op-banner.svg";
 import { Wrapper } from "./styles";
 
 const Header = () => {
+  
   return (
     <Wrapper>
-      <img className="logo" src={Logo} />
+      <Link to="/">
+        <img className="logo" id="logo" src={Logo} />
+      </Link>
       <img className="image" src={Banner} alt="One Piece original logo name" />
     </Wrapper>
   );
